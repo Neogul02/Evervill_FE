fc05239c7afa4bc6898da8de9ca8073a7f3a9486
Choe JinHyeong <wlsgud2414@naver.com>
2026-06-22 10:32:44 +0900

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from '@/router'
 
 const client = axios.create({
-  baseURL: '/',
+  baseURL: baseURL: import.meta.env.VITE_GATEWAY_URL || '/',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 })
